a21929274785ce86558f1bef13a0c329e965dafd
David Burns <david.burns@efront.com.au>
2021-03-31 15:07:27 +1100

    finishing signing handle submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils.js';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -15,10 +15,17 @@ class SignIn extends React.Component {
 			password:''
 		}
 	}
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
 
-		this.setState({email:'',password:''})
+		const { email, password } = this.state;
+		try{
+			await auth.signInWithEmailAndPassword(email,password)
+
+			this.setState({email: '', password: ''})
+		}catch(error){
+			console.log(error.message);
+		}
 	}
 
 	handleChange = event => {
